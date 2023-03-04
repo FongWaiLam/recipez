1d865826dcd96d6401365de16500296fe786fa16
Fong Wai Lam <2694026L@student.gla.ac.uk>
2023-03-04 02:38:59 +0000

    revised typo

--- recipez_project/asgi.py
+++ recipez_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for recipze_project project.
+ASGI config for recipez_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "recipze_project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "recipez_project.settings")
 
 application = get_asgi_application()
