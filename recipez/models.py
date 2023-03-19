27db94e238fafcba1c1a007bee1f54de6db3f7a7
satanb4 <sayan.bandyopadhyay@yahoo.co.in>
2023-03-19 20:55:05 +0000

    Added ingredients to be updated from many-to-many fields

--- recipez/models.py
+++ recipez/models.py
@@ -24,6 +24,11 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.user.username
 
+class Ingredient(models.Model):
+    name_and_amount = models.CharField(max_length=MAX_LENGTH, unique=True)
+
+    def __str__(self):
+        return self.name_and_amount
 
 class Recipe(models.Model):
     user = models.ForeignKey(
@@ -51,23 +56,11 @@ class Recipe(models.Model):
     is_active = models.BooleanField(default=True)
     cooking_duration = models.CharField(max_length=MAX_LENGTH)
     is_vegan = models.BooleanField(default=False)
-    # ingredients
+    ingredients = models.ManyToManyField(Ingredient)
     # comments
 
     def __str__(self):
         return self.name
-
-
-class Ingredient(models.Model):
-    recipes = models.ManyToManyField(
-        Recipe,
-        related_name='ingredients'
-    )
-
-    name_and_amount = models.CharField(max_length=MAX_LENGTH, unique=True)
-
-    def __str__(self):
-        return self.name_and_amount
 
 
 class Comment(models.Model):
