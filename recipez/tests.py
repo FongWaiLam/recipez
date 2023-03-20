c527533b495a1316d99f653dd024b4ed80be69be
Fong Wai Lam <2694026L@student.gla.ac.uk>
2023-03-20 03:42:07 +0000

    Corrected view for show_recipe (still unable to submit form), updated comment design

--- recipez/tests.py
+++ recipez/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
+
 
 # Create your tests here.
+
+class ShowRecipeViewTests(TestCase):
+    def test_show_recipe_view_with_no_recipe(self):
+
+        response = self.client.get(reverse('recipez:show_recipe'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, '')
+        self.assertQuerysetEqual(response.context['recipe'], None)
--- recipez/urls.py
+++ recipez/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('recipe/<recipe_id>/', views.show_recipe, name='show_recipe'),
     path('add_recipe/', views.add_recipe, name='add_recipe'),
-    path('add_comment/', views.add_comment, name='add_comment'),
     path('user_profile/', views.user_profile, name='user_profile'),
     path('login/', views.user_login, name='login'),
     path('register/', views.register, name='register'),
